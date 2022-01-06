6149c8604982ee497b9671516ca86b3ea93b0c2b
marine5575 <hope_insnow8@naver.com>
2022-01-06 23:51:43 +0900

    ~2-7. @babel/preset-env와 plugins

--- lecture/webpack.config.js
+++ lecture/webpack.config.js
@@ -11,6 +11,18 @@ module.exports = {
     entry: {
         app: ['./client'],
     },  // 입력
+
+    module: {
+        rules: [{
+            test: /\.jsx?/, // 규칙을 적용할 파일들
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env', '@babel/preset-react'],
+                plugins: ['@babel/plugin-proposal-class-properties'],
+            },
+        }],
+    },
+
     output: {
         path: path.join(__dirname, 'dist'), // C:\Users\jennie.ko\Desktop\react\lecture\dist
         filename: 'app.js',
