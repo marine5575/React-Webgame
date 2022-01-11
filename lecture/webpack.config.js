aa909a192fad7e71ca86772e8c4e2393335c3955
Merry Ko <43597548+marine5575@users.noreply.github.com>
2022-01-11 17:06:53 +0900

    ~6-6. Hooks에 대한 자잘한 팁들

--- lecture/webpack.config.js
+++ lecture/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const RefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 module.exports = {
-    name: '-setting',
+    name: 'lotto-setting',
     mode: 'development',
     devtool: 'eval',
     resolve: {
