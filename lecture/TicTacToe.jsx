b350f3442758f6319f8b6b0c1c36311003cfefa1
Merry Ko <43597548+marine5575@users.noreply.github.com>
2022-01-13 18:26:03 +0900

    ~7-3. action 만들어 dispatch 하기

--- lecture/TicTacToe.jsx
+++ lecture/TicTacToe.jsx
@@ -1,27 +1,66 @@
-import React, { useReducer, useState } from "react";
+import React, { useCallback, useReducer, useState } from "react";
 import Table from "./Table";
 
 const initialState = {
     winner: '',
     turn: 'O',
-    tableData: [['', '', ''], ['', '', ''], ['', '', '']],
+    tableData: [
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', '']
+    ],
 };
 
-const reducer = (state, action) => {
+export const SET_WINNER = 'SET_WINNER';
+export const CLICK_CELL = 'CLICK_CELL';
+export const SET_TURN = 'SET_TURN';
 
+const reducer = (state, action) => {
+    switch(action.type) {
+        case SET_WINNER: {
+            // state.winner = action.winner; 이렇게 하면 안 됨!
+
+            return {
+                ...state,
+                winner: action.winner,
+            };
+        }
+        case CLICK_CELL: {            
+            const tableData = [...state.tableData];
+
+            tableData[action.row] = [...tableData[action.row]]; // immer라는 library로 가독성 해결
+            tableData[action.row][action.cell] = state.turn;
+
+            return {
+                ...state,
+                tableData,
+            };
+        }
+
+        case SET_TURN: {
+            return {
+                ...state,
+                turn: state.turn === 'O' ? 'X' : 'O',
+            };
+        }
+    }
 };
 
 const TicTacToe = () => {
     const [state, dispatch] = useReducer(reducer, initialState);
 
-    const [winner, setWinner] = useState('');
-    const [turn, setTurn] = useState('O');
-    const [tableData, setTableData] = useState([['', '', ''], ['', '', ''], ['', '', '']]);
+    // const [winner, setWinner] = useState('');
+    // const [turn, setTurn] = useState('O');
+    // const [tableData, setTableData] = useState([['', '', ''], ['', '', ''], ['', '', '']]);
+
+    const onClickTable = useCallback(() => {
+        dispatch({ type: 'SET_WINNER', winner: 'O' });
+    }, []);
 
     return (
         <>
-            <Table />
-            {winner && <div>{winner}님의 승리</div>}
+            <Table onClick={onClickTable} tableData={state.tableData} dispatch={dispatch} />
+            {state.winner && <div>{state.winner}님의 승리</div>}
         </>
     );
 };
